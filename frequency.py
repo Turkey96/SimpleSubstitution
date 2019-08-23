8e687d006c8b67144842ea775d54f7e8ea2c1443
Yean Pabon <44075536+Turkey96@users.noreply.github.com>
2019-08-22 22:23:50 -0500

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,26 @@
+#TextoCifrado
+ciphertext = "PBFPVYFBQXZTYFPBFEQJHDXXQVAPTPQJKTOYQWIPBVWLXTOXBTFXQWAXBVCXQWAXFQJVWLEQNTOZQGGQLFXQWAKVWLXQWAEBIPBFXFQVXGTVJVWLBTPQWAEBFPBFHCVLXBQUFEVWLXGDPEQVPQGVPPBFTIXPFHXZHVFAGFOTHFEFBQUFTDHZBQPOTHXTYFTODXQHFTDPTOGHFQPBQWAQJJTODXQHFOQPWTBDHHIXQVAPBFZQHCFWPFHPBFIPBQWKFABVYYDZBOTHPBQPQJTQOTOGHFQAPBFEQJHDXXQVAVXEBQPEFZBVFOJIWFFACFCCFHQWAUVWFLQHGFXVAFXQHFUFHILTTAVWAFFAWTEVOITDHFHFQAITIXPFHXAFQHEFZQWGFLVWPTOFFA"
+#Funcion para contar el numero de veces que se repite un caracter
+def frequency(ciphertext):
+    all_freq = {} 
+    for i in ciphertext: 
+        if i in all_freq: 
+            all_freq[i] += 1
+        else: 
+            all_freq[i] = 1
+    #Mostrar resultados
+    print ("Numero de repeticiones por caracater:\n "+ str(all_freq))
+    return 0
+#Lllamdo de la funcion
+frequency(ciphertext)
+intab = "ABCDEFGHIJKLNOPQTUVWXYZ"
+key=input("Inserte llave, notese que los valores introducidos seran relacionados con los caracteres en orden alfabetico y la llave debe ser de la misma extension del numero de caracteres en el texto cifrado\n"+intab+"\n")
+#Datos para crear tabla de traduccion
+
+outtab = key#Clave para descifrar "DHPUWEBRYLKGXFTAOVINSMC"
+#Crear tabla de traduccion
+trantab = str.maketrans(intab, outtab)
+#Texto a traducir
+str = "PBFPVYFBQXZTYFPBFEQJHDXXQVAPTPQJKTOYQWIPBVWLXTOXBTFXQWAXBVCXQWAXFQJVWLEQNTOZQGGQLFXQWAKVWLXQWAEBIPBFXFQVXGTVJVWLBTPQWAEBFPBFHCVLXBQUFEVWLXGDPEQVPQGVPPBFTIXPFHXZHVFAGFOTHFEFBQUFTDHZBQPOTHXTYFTODXQHFTDPTOGHFQPBQWAQJJTODXQHFOQPWTBDHHIXQVAPBFZQHCFWPFHPBFIPBQWKFABVYYDZBOTHPBQPQJTQOTOGHFQAPBFEQJHDXXQVAVXEBQPEFZBVFOJIWFFACFCCFHQWAUVWFLQHGFXVAFXQHFUFHILTTAVWAFFAWTEVOITDHFHFQAITIXPFHXAFQHEFZQWGFLVWPTOFFA"
+#Mostrar texto traducido
+print (str.translate(trantab))
